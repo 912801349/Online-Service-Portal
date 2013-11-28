c2b2186986543b0cdef8b8592310d0423cfb59bd
bhavanacse <bhavana@ubuntu.(none)>
2013-11-28 12:09:40 -0800

    Changes to Gemfile to add pg as Production database

--- Gemfile
+++ Gemfile
@@ -9,7 +9,9 @@ gem 'rails', '4.0.0'
 gem 'devise', :git => 'git://github.com/plataformatec/devise.git', :ref => '49aebde'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+group :development do
+  gem 'sqlite3'
+end
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
@@ -43,6 +45,11 @@ gem 'jbuilder', '~> 1.2'
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
+end
+
+group :production do
+  gem 'pg', '0.15.1'
+  gem 'rails_12factor', '0.0.2'
 end
 
 # Use ActiveModel has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,21 @@ ActiveRecord::Schema.define(version: 20131124043306) do
     t.string   "comment"
   end
 
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "providers", force: true do |t|
     t.string   "username",               default: "", null: false
